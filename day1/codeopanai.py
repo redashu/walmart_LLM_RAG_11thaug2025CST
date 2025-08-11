e6a4e893ac369c1c43385941ecfd3b162a56a296
Ashutoshh <ashutoshh@linux.com>
2025-08-12 00:20:23 +0530

    updates LLM + rag

--- day1/codeopanai.py
+++ day1/codeopanai.py
@@ -4,8 +4,9 @@ ashu_key = "sk-ashu1234567890abcdefg"  # Replace with your actual API key
 client = OpenAI(api_key=ashu_key)
 
 response = client.responses.create(
-    model="gpt-5-nano",
-    input="Write a short bedtime story about a unicorn."
+    model="gpt-4.1-nano",
+    input="Write a short bedtime story about a unicorn in 15 words.",
+    max_output_tokens=20
 )
 
 print(response.output_text)
